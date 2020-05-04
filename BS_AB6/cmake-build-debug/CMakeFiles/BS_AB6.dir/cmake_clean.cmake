dbab07d511273c07ef99d5a7c582540198cedbae
christina447 <christinabernhardt.cb@gmail.com>
2020-05-04 02:26:36 +0200

    first upload

---
+++ BS_AB6/cmake-build-debug/CMakeFiles/BS_AB6.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "BS_AB6"
+  "BS_AB6.pdb"
+  "CMakeFiles/BS_AB6.dir/main.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/BS_AB6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ BS_AB6/main.c
@@ -0,0 +1,124 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <string.h>
+
+#define QUEUESIZE 20
+#define MAXCHAR 50
+
+//-------------------------------QUEUE-------------------------------------------------------
+//Queue Struct
+typedef struct {
+    char *buffer[QUEUESIZE];
+    int head, tail;
+    int full, empty;
+} queue;
+
+//Queue erzeugen
+queue *queueInit (void){
+
+    queue *q;
+    q=(queue *)malloc (sizeof ( queue));
+    if (q == NULL){
+        return (NULL);
+    }
+    q->empty = 1;
+    q->full = 0;
+    q->head = 0;
+    q->tail = 0;
+}
+
+//Objekte in die Queue einfügen
+void addInQ(queue *q, char *in){
+
+    char* dup = strdup(in);
+
+    q->buffer[q->tail] = dup;
+    q->tail++;
+    if (q->tail == QUEUESIZE){
+        q->tail = 0;
+    }
+    if (q->tail == q->head){
+        q->full = 1;
+    }
+    return;
+}
+
+//Entnehmen bzw. löschen von Objekten aus der Queue
+char* delFromQ(queue *q){
+
+    char *out = q->buffer[q->head];
+
+    q->head++;
+    if (q->head == QUEUESIZE){
+        q->head = 0;
+    }
+    if (q->head == q->tail){
+        q->empty = 1;
+    }
+    q->full = 0;
+    return out;
+}
+
+// -----------------------------FILE-READER-----------------------------------------
+void fileReader(queue **fifo) {
+    char fileName[50];
+    FILE *fptr = NULL;
+    char line[MAXCHAR];
+    char *c;
+
+    printf("Name des einzulesenden Files angeben:\n");
+    scanf("%s", fileName);
+
+    fptr = fopen(fileName, "r");
+    if (fptr == NULL) {
+        printf("Einlesen der Datei nicht möglich");
+        return;
+    }
+
+    while (fgets(line, MAXCHAR, fptr) != NULL) {
+       // if (*c = strchr(line, '\n') != NULL){
+       //     *c = '\0';
+     //   }
+        addInQ(fifo, line);
+    }
+}
+
+//--------------------------------MAIN------------------------------------------------------------
+int main() {
+
+    queue *fifo;
+    char *output ;
+    pthread_t readerThread;
+
+    //Erzeugen der Queue
+    fifo = queueInit();
+    if (fifo == NULL){
+        printf ("Fehler beim initialisieren der Queue");
+    }
+
+    //Einlesen des Files durch den Reader-Thread
+    pthread_create(&readerThread, NULL, fileReader, fifo);   //Testen der Threads
+
+   // fileReader(fifo);
+
+    pthread_join(readerThread, NULL);
+    output = delFromQ(fifo);
+    printf("out: %s", output);
+    output = delFromQ(fifo);
+    printf("out: %s", output);
+    output = delFromQ(fifo);
+    printf("out: %s", output);
+    output = delFromQ(fifo);
+    printf("out: %s", output);
+    output = delFromQ(fifo);
+    printf("out: %s", output);
+    output = delFromQ(fifo);
+    printf("out: %s", output);
+    output = delFromQ(fifo);
+    printf("out: %s", output);
+
+
+}
+
+
